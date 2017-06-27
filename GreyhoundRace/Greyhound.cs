649dbf168c03a23d413ad96769f2548d42149214
Mateusz Czech <mateuszczech@live.co.uk>
2017-06-27 07:38:49 +0200

    Added Greyhound class.

---
+++ GreyhoundRace/Greyhound.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreyhoundRace
+{
+    public class Greyhound
+    {
+        public int StartingPosition;
+        public int RacetrackLenght;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random MyRandom;
+
+        public bool Run()
+        {
+            MyPictureBox.Left += MyRandom.Next(1, 5);
+            if(MyPictureBox.Left >= RacetrackLenght - MyPictureBox.Width)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void TakeStartingPosition()
+        {
+
+        }
+    }
+}
