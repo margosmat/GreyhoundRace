54b66ab40cc7e771b96a0a9dd8c3c30ba6579402
Mateusz Czech <mateuszczech@live.co.uk>
2017-06-30 07:21:50 +0200

    Added class Bet.

---
+++ GreyhoundRace/Bet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyhoundRace
+{
+    public class Bet
+    {
+        public int Amount;
+        public int Dog;
+        public Guy Bettor;
+
+
+    }
+}
